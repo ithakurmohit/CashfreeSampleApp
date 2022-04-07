992316ab346ef0a84ee2789aba0ec8877dfdc7cf
mohitrbl <103178205+mohitrbl@users.noreply.github.com>
2022-04-07 14:07:27 +0530

    Update MainActivity.java

--- app/src/main/java/com/gocashfree/cfsdk/sample/cfsdksample/MainActivity.java
+++ app/src/main/java/com/gocashfree/cfsdk/sample/cfsdksample/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         Log.d(TAG," Testing github");
     }
 
     @Override
